b84af904610b38d70c3520d4c162ddf4333f9a90
TheInfernalNick <theinfernalnick@gmail.com>
2023-12-28 15:16:56 +0100

    LittleSupernova
    For now the Supernova Algorithm functions in a really simple and basic way, it's not really much scalable and efficient, IT ISNT THE LAST VERSION

--- web/src/algorithms/supernova.ts
+++ web/src/algorithms/supernova.ts
@@ -1,6 +1,46 @@
 import neo4j from '@config/neo4j.config'
 
 export const supernova = async (user: string) => {
-   let query = `MATCH (u:User) WHERE u.name = ${user} RETURN u`
-   if (neo4j) neo4j.run(query)
+
+   //DEFINING THE VARIABELS NEEDED TO WORK
+   let startingPoint: string | undefined = user
+   let friendList: Array<string> = []
+   let alreadySearched: Array<string> = []
+   let friend: string | undefined = ""
+
+   let queryFriends = `MATCH (u:User)-[:Friend]-(t:User) WHERE u.name = "${startingPoint}" RETURN t`
+
+   //TODO: COMMENTA TUTTO SENNò NON CI CAPISCI NA SEGA
+   if (neo4j) {
+      const result = await neo4j.executeRead(tx => tx.run(queryFriends))
+      let results = result.records.map(row => row.get('t'))
+      results.forEach(element => {
+         friendList.push(element.properties["name"])
+      });
+
+
+      while (friendList.length > 0) {
+
+         startingPoint = friendList.shift()
+         //QUERY PER GLI AMICI
+         queryFriends = `MATCH (u:User)-[:Friend]-(t:User) WHERE u.name = "${startingPoint}" RETURN t`
+
+         const result1 = await neo4j.executeRead(tx => tx.run(queryFriends))
+         results = result1.records.map(row => row.get('t'))
+
+         for await (let element of results) {
+            friend = element.properties["name"]
+            //QUERY PER VEDERE SE SONO GIà AMICI
+            const queryAlreadyFriend = `OPTIONAL MATCH (u:User)-[:Friend]-(t:User) WHERE u.name="${user}" AND t.name="${friend}" RETURN t`
+            const result = await neo4j?.executeRead(tx => tx.run(queryAlreadyFriend))
+            let testNull = result.records.map(row => row.get('t'))
+            console.log(testNull[0])
+            if (testNull[0] === null) {
+               console.log(friend)
+               return friend
+            }
+         }
+      }
+   }
+
 }
--- web/src/routes/user/route.ts
+++ web/src/routes/user/route.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { supernova } from "../../algorithms/supernova";
 
 const app: Router = Router();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
    console.log(res.locals.jwt);
-   res.json({});
+
+   res.json({
+      "uptime": process.uptime(),
+      "status": 200,
+      "platform": process.platform,
+      "result": await supernova("Test")
+   });
 });
 
 export default app;
