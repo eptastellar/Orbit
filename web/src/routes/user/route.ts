fce78c90b6092c0123aca7f25a0aa19efeb19502
TheInfernalNick <theinfernalnick@gmail.com>
2023-12-28 15:16:56 +0100

    LittleSupernova
    For now the Supernova Algorithm functions in a really simple and basic way, it's not really much scalable and efficient, IT ISNT THE LAST VERSION

--- web/src/routes/user/route.ts
+++ web/src/routes/user/route.ts
@@ -1,9 +1,17 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+import { supernova } from "../../algorithms/supernova";
+
 const app: Router = Router();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-   console.log(res.locals.uid);
-   res.json({});
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+   console.log(res.locals.jwt);
+
+   res.json({
+      "uptime": process.uptime(),
+      "status": 200,
+      "platform": process.platform,
+      "result": await supernova("Test")
+   });
 });
 
 export default app;
