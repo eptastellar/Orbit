a4be4133fcfee8e72a564bf149000f1c71eb6d62
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-12-29 15:12:50 +0100

    Vite init

---
+++ app/src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => (
+   <div className="flex items-center justify-center h-screen w-screen bg-black">
+      <p className="text-6xl font-bold text-white">Orbit</p>
+   </div>
+)
+
+export default App
