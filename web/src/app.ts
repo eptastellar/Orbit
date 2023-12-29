63ce9b15e290df10ce58a2f4934557983828ff80
mzyxnuel <sossai-m@proton.me>
2023-12-29 23:08:08 +0100

    fix: routing paths

--- web/src/app.ts
+++ web/src/app.ts
@@ -3,18 +3,18 @@ import express, { Express } from 'express';
 import { middleware } from '../src/helpers/middleware';
 import signup from './routes/auth/sign-up/route';
 import health from './routes/health/route';
-import user from './routes/user/route';
 import interests from './routes/interests/route';
+import user from './routes/user/route';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use('/health', health)
-app.use('/auth/sign-up', middleware, signup)
-app.use('/interests', interests)
-app.use('/user', middleware, user)
+app.use('/api/health', health)
+app.use('/api/auth/sign-up', middleware, signup)
+app.use('/api/interests', interests)
+app.use('/api/user', middleware, user)
 
 app.listen(port, () => { console.log(`⚡[server]: server is running on port: http://localhost:${port}`) }) //TODO REMOVE IN PRODUCTION
 
