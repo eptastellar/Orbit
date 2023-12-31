009032bbac3e0ab6eb642572b601ae46e021acf2
TheInfernalNick <theinfernalnick@gmail.com>
2023-12-31 16:28:37 +0100

    Neo4j user add ( not already working )
    We will need to understand how we pass the data to the backend to make this query work, for now it has blank data, at given time it will work with the given data

--- web/src/algorithms/supernova.ts
+++ web/src/algorithms/supernova.ts
@@ -2,5 +2,5 @@ import neo4j from '../lib/neo4j.config'
 
 export const supernova = async (user: string) => {
    let query = `MATCH (u:User) WHERE u.name = ${user} RETURN u`
-   neo4j.run(query)
+   if (neo4j) neo4j.run(query)
 }
