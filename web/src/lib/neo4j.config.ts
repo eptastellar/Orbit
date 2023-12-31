70fcf07330ba0fddbae672465834dce10b2bef7f
TheInfernalNick <theinfernalnick@gmail.com>
2023-12-31 16:28:37 +0100

    Neo4j user add ( not already working )
    We will need to understand how we pass the data to the backend to make this query work, for now it has blank data, at given time it will work with the given data

--- web/src/lib/neo4j.config.ts
+++ web/src/lib/neo4j.config.ts
@@ -1,12 +1,21 @@
-import neo4j from 'neo4j-driver'
+import dotenv from 'dotenv';
+import neo4j, { Driver, Session } from 'neo4j-driver';
 
-const NEO4J_URI = process.env.NEO4J_URI
-const NEO4J_USERNAME = process.env.NEO4J_USERNAME
-const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD
+dotenv.config()
 
-export const driver = neo4j.driver(
-   NEO4J_URI,
-   neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
-)
+const NEO4J_URI = process.env.NEO4J_URI;
+const NEO4J_USERNAME = process.env.NEO4J_USERNAME;
+const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD;
 
-export default driver.session()
+let driver: Driver | null = null;
+let session: Session | null = null;
+if (NEO4J_URI && NEO4J_USERNAME && NEO4J_PASSWORD) {
+   driver = neo4j.driver(
+      NEO4J_URI,
+      neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
+   );
+
+   session = driver.session();
+}
+
+export default session;
--- web/src/routes/auth/sign-up/route.ts
+++ web/src/routes/auth/sign-up/route.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response, Router } from "express";
+import neo4j from "../../../lib/neo4j.config";
 
 const app: Router = Router();
 
 app.post("/", (req: Request, res: Response, next: NextFunction) => {
-   //TODO @TheInfernalNick crea nodo
-
+   //TODO @Mxzynvuel dammi dati veri cosi so come aggiungere i dati nella query effettivamente
+   let interests = ""
+   let bday = ""
+   const query = `CREATE (:User {name:"${name}",interests:${interests},bday:${bday}})`
+   if (neo4j) {
+      const result = neo4j.executeWrite(tx => tx.run(query))
+   }
    // res.json({ success: true, message: "Node created" }).status(201);
    res.json({ success: false, message: "Error while creating the node" }).status(400);
 });
