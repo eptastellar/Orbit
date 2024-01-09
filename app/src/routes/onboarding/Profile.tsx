3bf6543bf53983473742265a0291f8eceae74c81
74C17N3P7UN3 <leocalzavara05@gmail.com>
2024-01-09 19:26:46 +0100

    Finished profile page
    Still needs API validation

--- app/src/routes/onboarding/Profile.tsx
+++ app/src/routes/onboarding/Profile.tsx
@@ -1,21 +1,37 @@
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage"
 import { useState } from "react"
 import { PiCameraPlus } from "react-icons/pi"
 import { useNavigate } from "react-router-dom"
 
 import { BackButton, Input } from "@/components"
 import { Wrapper } from "@/hoc"
+import { storage } from "@/libraries/firebase"
 
 const Profile = () => {
    const navigateTo = useNavigate()
 
    const [error, setError] = useState<string>("")
+   const [progress, setProgress] = useState<number>(0)
 
-   const [profilePictureUrl, setProfilePictureUrl] = useState<string>("")
-   const [username, setUsername] = useState<string>("")
-   const [birthdate, setBirthdate] = useState("")
+   const [profilePictureUrl, setProfilePictureUrl] = useState<string>(localStorage.getItem("profilePicture") ?? "")
+   const [username, setUsername] = useState<string>(localStorage.getItem("username") ?? "")
+   const [birthdate, setBirthdate] = useState(localStorage.getItem("birthdate") ?? "")
 
-   const handleUpload = (event: React.FormEvent) => {
-      //
+   const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const file = event.target.files ? event.target.files[0] : null
+
+      if (file) {
+         const uploadTask = uploadBytesResumable(ref(storage, `uploads/pfps/${crypto.randomUUID()}`), file)
+
+         uploadTask.on("state_changed",
+            (snapshot) => setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100),
+            (error) => setError(error.message),
+            () => getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+               localStorage.setItem("profilePicture", downloadURL)
+               setProfilePictureUrl(downloadURL)
+            })
+         )
+      }
    }
 
    const updateUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,9 +56,17 @@ const Profile = () => {
    const handleSubmit = async (event: React.FormEvent) => {
       event.preventDefault()
 
+      if (birthdate.length !== 10)
+         return setError("Invalid birthdate.")
+
       try {
+         const unixBirthdate = Math.floor(new Date(birthdate).getTime() / 1000)
+         if (!unixBirthdate) return setError("Invalid birthdate.")
+
          // TODO: Check form validity with api endpoint
-         // TODO: Save data to localstorage
+
+         localStorage.setItem("username", username)
+         localStorage.setItem("birthdate", birthdate)
          navigateTo("/onboarding/interests")
       } catch (error: any) { setError(error.message) }
    }
@@ -62,10 +86,23 @@ const Profile = () => {
             className="flex flex-col center gap-4 w-full"
             onSubmit={(event) => handleSubmit(event)}
          >
-            <label className="flex center h-32 w-32 ring-inset ring-1 ring-gray-5 bg-gray-7 rounded-full">
-               <PiCameraPlus className="text-white text-5xl" />
+            <label
+               className={`flex center h-32 w-32 ${progress === 0 ? "p-[1px]" : "p-1"} rounded-full transition-all duration-500`}
+               style={{
+                  background: profilePictureUrl ? "#1D5C96" :
+                     `conic-gradient(#1D5C96 0deg, #1D5C96 ${Math.floor(progress * 3.6)}deg, #585858 ${Math.floor(progress * 3.6)}deg)`
+               }}
+            >
+               <div className="flex center h-full w-full bg-gray-7 rounded-full overflow-hidden">
+                  {profilePictureUrl ? (
+                     <img src={profilePictureUrl} alt="Profile picture" className="h-full w-full object-cover" />
+                  ) : (
+                     <PiCameraPlus className="text-white text-5xl" />
+                  )}
+               </div>
                <input
                   type="file"
+                  accept="image/gif, image/jpeg, image/png"
                   onChange={(event) => handleUpload(event)}
                   className="hidden"
                />
