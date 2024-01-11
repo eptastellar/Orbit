b135ed58c32bb449fe914bbfb7ee4ca0276edb43
mzyxnuel <sossai-m@proton.me>
2024-01-11 22:21:05 +0100

    Update validate.ts

--- web/src/helpers/validate.ts
+++ web/src/helpers/validate.ts
@@ -4,7 +4,7 @@ import { Firestore, Query, QuerySnapshot } from 'firebase-admin/firestore';
 
 export function isValidBday(bday: number): Promise<null> {
    return new Promise((resolve, reject) => {
-      if (bday > Date.now() || bday < -2208988800)
+      if (!bday || bday > Date.now() || bday < -2208988800)
          reject(new Error('validation/invalid-birthdate'))
 
       if (((Date.now() - 441806400) - bday) <= 0)
@@ -17,18 +17,19 @@ export function isValidBday(bday: number): Promise<null> {
 export async function isValidUsername(username: string): Promise<null> {
    baas()
    const db: Firestore = admin.firestore();
-   const usersRef: Query = db.collection('users').where("username", "==", username); //search where the username is equal to the input username
 
    return new Promise((resolve, reject) => {
-      if (username.length > 24)
+      const regex = /[^a-zA-Z0-9\_\-\.]/
+      if (!username || !username.startsWith('@') || username.substring(1).match(regex))
+         reject(new Error('validation/invalid-username'))
+
+      if (username.length > 24 + 1)
          reject(new Error('validation/username-too-long'))
 
-      if (username.length < 6)
+      if (username.length < 6 + 1)
          reject(new Error('validation/username-too-short'))
 
-      const regex = /'[^a-zA-Z0-9_\-.]'/
-      if (username.match(regex) || !username.startsWith('@'))
-         reject(new Error('validation/invalid-username'))
+      const usersRef: Query = db.collection('users').where("username", "==", username); //search where the username is equal to the input username
 
       usersRef.get()
          .then(async (snapshot: QuerySnapshot) => {
