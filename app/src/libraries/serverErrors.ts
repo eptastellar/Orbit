765f094cc5391ba91f4d39ea5555a976e5ef8bb8
74C17N3P7UN3 <leocalzavara05@gmail.com>
2024-01-11 22:56:21 +0100

    Added profile verification
    Co-Authored-By: mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
    Co-Authored-By: TheInfernalNick <82029415+TheInfernalNick@users.noreply.github.com>

---
+++ app/src/libraries/serverErrors.ts
@@ -0,0 +1,12 @@
+export const resolveServerError = (error: string): string => {
+   // let resolvedError = ""
+
+   // if (error.includes("validation/username-too-long"))
+   //    resolvedError = "Email address already in use."
+   // else if (error.includes("auth/invalid-credential"))
+   //    resolvedError = "Provided invalid credentials."
+   // else resolvedError = error
+
+   // return resolvedError
+   return error
+}
--- app/src/routes/onboarding/Profile.tsx
+++ app/src/routes/onboarding/Profile.tsx
@@ -3,13 +3,15 @@ import { useState } from "react"
 import { PiCameraPlus } from "react-icons/pi"
 import { useNavigate } from "react-router-dom"
 
-import { BackButton, Input } from "@/components"
+import { BackButton, Input, SpinnerText } from "@/components"
 import { Wrapper } from "@/hoc"
 import { storage } from "@/libraries/firebase"
+import { resolveServerError } from "@/libraries/serverErrors"
 
 const Profile = () => {
    const navigateTo = useNavigate()
 
+   const [loading, setLoading] = useState<boolean>(false)
    const [error, setError] = useState<string>("")
    const [progress, setProgress] = useState<number>(0)
 
@@ -21,6 +23,9 @@ const Profile = () => {
       const file = event.target.files ? event.target.files[0] : null
 
       if (file) {
+         setProfilePictureUrl("")
+         localStorage.removeItem("profilePicture")
+
          const uploadTask = uploadBytesResumable(ref(storage, `uploads/pfps/${crypto.randomUUID()}`), file)
 
          uploadTask.on("state_changed",
@@ -59,16 +64,41 @@ const Profile = () => {
       if (birthdate.length !== 10)
          return setError("Invalid birthdate.")
 
+      setLoading(true)
+
       try {
          const unixBirthdate = Math.floor(new Date(birthdate).getTime() / 1000)
          if (!unixBirthdate) return setError("Invalid birthdate.")
 
-         // TODO: Check form validity with api endpoint
+         // Check form validity with api endpoint
+         const requestBody = JSON.stringify({
+            username: username,
+            bday: unixBirthdate
+         })
 
-         localStorage.setItem("username", username)
-         localStorage.setItem("birthdate", birthdate)
-         navigateTo("/onboarding/interests")
-      } catch (error: any) { setError(error.message) }
+         const params: RequestInit = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: requestBody
+         }
+
+         type ResponseType = { success: boolean, message: string }
+         fetch(`${import.meta.env.VITE_API_URL}/auth/sign-up/validate`, params)
+            .then((response) => response.json())
+            .then(({ success, message }: ResponseType) => {
+               if (success) {
+                  setError("")
+
+                  localStorage.setItem("username", username)
+                  localStorage.setItem("birthdate", birthdate)
+                  navigateTo("/onboarding/interests")
+               } else setError(resolveServerError(message))
+            })
+            .finally(() => setLoading(false))
+      } catch (error: any) {
+         setError(error.message)
+         setLoading(false)
+      }
    }
 
    return (
@@ -108,18 +138,13 @@ const Profile = () => {
                />
             </label>
 
-            <div className="flex flex-col items-end justify-center gap-1 w-full">
-               <Input
-                  label="Username"
-                  placeholder="@4stroNebul4"
-                  type="text"
-                  value={username}
-                  onChange={(event) => updateUsername(event)}
-               />
-               <p className="text-[10px] font-medium text-gray-3">
-                  You won't be able to change this later.
-               </p>
-            </div>
+            <Input
+               label="Username"
+               placeholder="@4stroNebul4"
+               type="text"
+               value={username}
+               onChange={(event) => updateUsername(event)}
+            />
             <div className="flex flex-col items-end justify-center gap-1 w-full">
                <Input
                   label="Birthdate"
@@ -139,7 +164,7 @@ const Profile = () => {
                type="submit"
                className="w-full py-2 text-base font-semibold text-white bg-blue-7 rounded-md"
             >
-               Save your profile
+               {loading ? <SpinnerText message="Building your rocket..." /> : <p>Save your profile</p>}
             </button>
          </form>
 
