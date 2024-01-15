0c1b0eb11e842ffc71f996148056d1d40b03c7d8
74C17N3P7UN3 <leocalzavara05@gmail.com>
2024-01-15 18:04:14 +0100

    Renamed to verification

--- app/src/routes/onboarding/Confirmation.tsx
+++
@@ -1,9 +0,0 @@
-import { Wrapper } from "@/hoc"
-
-const Confirmation = () => (
-   <div className="flex center h-full w-full">
-      <p className="text-white">Confirmation</p>
-   </div>
-)
-
-export default Wrapper({ children: <Confirmation />, needsAuth: true, isConfirmPage: true })
