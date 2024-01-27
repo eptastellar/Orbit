3e491b225009aff9bb2b848b975e1470d53f8a4a
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-01-27 21:21:34 +0100

    enhance(validation): added try catch for each validation

--- web/src/helpers/validate.ts
+++ web/src/helpers/validate.ts
@@ -7,68 +7,104 @@ const db: Firestore = firestore()
 //TODO: add text and image control for harmful behavior
 export function isValidBday(bday: number): Promise<null> {
    return new Promise((resolve, reject) => {
-      if (!bday || bday > Date.now() / 1000 || bday < -2208988800)
-         reject(new Error('validation/invalid-birthdate'))
+      try {
+         if (!bday || bday > Date.now() / 1000 || bday < -2208988800)
+            reject(new Error('validation/invalid-birthdate'))
 
-      if (((Date.now() / 1000 - 441806400) - bday) <= 0)
-         reject(new Error('validation/too-young'))
+         if (((Date.now() / 1000 - 441806400) - bday) <= 0)
+            reject(new Error('validation/too-young'))
 
-      resolve(null)
+         resolve(null)
+      } catch (_) {
+         reject(new Error('validation/invalid-content-type'))
+      }
    })
 }
 
 export async function isValidUsername(username: string): Promise<null> {
    return new Promise((resolve, reject) => {
-      const regex: RegExp = /[^a-zA-Z0-9\_\-\.]/
+      try {
+         const regex: RegExp = /[^a-zA-Z0-9\_\-\.]/
 
-      if (!username || !username.startsWith('@') || username.substring(1).match(regex)) //check if the username i approved by the regex
-         reject(new Error('validation/invalid-username'))
+         if (!username || !username.startsWith('@') || username.substring(1).match(regex)) //check if the username i approved by the regex
+            reject(new Error('validation/invalid-username'))
 
-      if (username.length > 24 + 1)
-         reject(new Error('validation/username-too-long'))
+         if (username.length > 24 + 1)
+            reject(new Error('validation/username-too-long'))
 
-      if (username.length < 6 + 1)
-         reject(new Error('validation/username-too-short'))
+         if (username.length < 6 + 1)
+            reject(new Error('validation/username-too-short'))
 
-      const usersRef: Query = db.collection('users').where('username', '==', username) //search where the username is equal to the input username
+         const usersRef: Query = db.collection('users').where('username', '==', username) //search where the username is equal to the input username
 
-      usersRef.get().then(async (snapshot: QuerySnapshot) => {
-         if (snapshot.empty) //check if username is already used
-            resolve(null)
-         else reject(new Error('validation/username-already-in-use'))
-      })
+         usersRef.get().then(async (snapshot: QuerySnapshot) => {
+            if (snapshot.empty) //check if username is already used
+               resolve(null)
+            else reject(new Error('validation/username-already-in-use'))
+         })
+      } catch (_) {
+         reject(new Error('validation/invalid-content-type'))
+      }
    })
 }
 
 export async function areValidInterests(interestsList: string[]): Promise<null> {
    return new Promise((resolve, reject) => {
-      if (interests.length > 5 && interests.length < 1)
-         reject(new Error('validation/invalid-number-of-interests'))
+      try {
+         if (interests.length > 5 && interests.length < 1)
+            reject(new Error('validation/invalid-number-of-interests'))
 
-      interestsList.forEach(element => {
-         if (!interests.includes(element))
-            reject(new Error('validation/invalid-interests'))
-      })
-      resolve(null)
+         interestsList.forEach(element => {
+            if (!interests.includes(element))
+               reject(new Error('validation/invalid-interests'))
+         })
+         resolve(null)
+      } catch (_) {
+         reject(new Error('validation/invalid-content-type'))
+      }
    })
 }
 
-export async function isValidLastDocId(lastDocId: string): Promise<null> {
+export async function isValidDocId(docId: string): Promise<null> {
    return new Promise(async (resolve, reject) => {
-      if (lastDocId) {
-         const docRef = await db.collection('posts').doc(lastDocId).get()
+      try {
+         if (docId) {
+            const docRef = await db.collection('posts').doc(docId).get()
 
-         if (docRef.exists)
-            resolve(null)
-         else reject(new Error('validation/invalid-last-doc-id'))
-      } else resolve(null)
+            if (docRef.exists)
+               resolve(null)
+         } else resolve(null)
+      } catch (_) {
+         reject(new Error('validation/invalid-doc-id'))
+      }
+   })
+}
+
+export async function isValidCommentRootId(rootId: string, postId: string): Promise<null> {
+   return new Promise(async (resolve, reject) => {
+      try {
+         const docRef = await db.collection('comments').doc(rootId).get()
+
+         if (docRef.exists) {
+            const postRef = await db.collection('posts').doc(postId).get()
+            if (docRef.data()?.owner == postRef.data()?.owner)
+               resolve(null)
+            else reject(new Error('validation/invalid-doc-id'))
+         } else reject(new Error('validation/invalid-doc-id'))
+      } catch (_) {
+         reject(new Error('validation/invalid-doc-id'))
+      }
    })
 }
 
 export async function isValidContentType(type: string): Promise<null> {
    return new Promise((resolve, reject) => {
-      if (type == "text" || type == "image" || type == "audio")
-         resolve(null)
-      reject(new Error('validation/invalid-content-type'))
+      try {
+         if (type == "text" || type == "image" || type == "audio")
+            resolve(null)
+         reject(new Error('validation/invalid-content-type'))
+      } catch (_) {
+         reject(new Error('validation/invalid-content-type'))
+      }
    })
 }
