4da1142be48b00806f152379fc6a4977e66497ee
74C17N3P7UN3 <leocalzavara05@gmail.com>
2024-01-28 22:33:58 +0100

    Fixed 404 page
    Now it renders as a server component

--- app/src/app/not-found.tsx
+++ app/src/app/not-found.tsx
@@ -1,32 +1,18 @@
-"use client"
+import { BackButton } from "@/components"
 
-import { useRouter } from "next/navigation"
-import { PiArrowLeftBold } from "react-icons/pi"
-
-const ErrorPage = () => {
-   // Next router for navigation
-   const router = useRouter()
-
-   return (
-      <div className="flex flex-col gap-12 center h-screen w-screen px-8 bg-black">
-         <div>
-            <h1 className="text-9xl font-black text-black text-stroke">
-               404
-            </h1>
-            <p className="text-center text-base text-white">
-               page not found.
-            </p>
-         </div>
-
-         <button
-            className="flex gap-2 center w-64 px-4 py-2 text-black bg-white rounded-md cursor-pointer"
-            onClick={() => router.back()}
-         >
-            <PiArrowLeftBold />
-            <span>Go back</span>
-         </button>
+const ErrorPage = () => (
+   <div className="flex flex-col gap-12 center h-screen w-screen px-8 bg-black">
+      <div>
+         <h1 className="text-9xl font-black text-black text-stroke">
+            404
+         </h1>
+         <p className="text-center text-base text-white">
+            page not found.
+         </p>
       </div>
-   )
-}
+
+      <BackButton colors="text-black bg-white" text="Go back" />
+   </div>
+)
 
 export default ErrorPage
--- app/src/components/button/BackButton.tsx
+++ app/src/components/button/BackButton.tsx
@@ -1,16 +1,23 @@
-import { useRouter } from "next/navigation"
-import { PiArrowLeft } from "react-icons/pi"
+"use client"
 
-const BackButton = () => {
+import { useRouter } from "next/navigation"
+import { PiArrowLeftBold } from "react-icons/pi"
+
+type Props = {
+   colors?: string
+   text?: string
+}
+
+const BackButton = ({ colors = "text-white bg-blue-7", text = "Back" }: Props) => {
    const router = useRouter()
 
    return (
       <button
-         className="flex gap-2 center px-4 py-2 text-white bg-blue-7 rounded-md cursor-pointer"
+         className={`flex gap-2 center px-4 py-2 ${colors} rounded-md cursor-pointer`}
          onClick={() => router.back()}
       >
-         <PiArrowLeft />
-         <p className="text-base font-semibold">Back</p>
+         <PiArrowLeftBold />
+         <p className="text-base font-semibold">{text}</p>
       </button>
    )
 }
