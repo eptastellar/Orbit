face55eea6f1f2a295a397c9cea0b195593e15f6
74C17N3P7UN3 <leocalzavara05@gmail.com>
2024-02-02 22:36:58 +0100

    Removed confetti
    Until a later date

--- app/src/app/(app)/u/[username]/page.tsx
+++ app/src/app/(app)/u/[username]/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import ConfettiExplosion, { ConfettiProps } from "react-confetti-explosion"
 
 type Props = {
    params: {
@@ -17,32 +16,15 @@ const User = ({ params }: Props) => {
    const [loading, setLoading] = useState<boolean>(true)
 
    // Interaction states
-   const [deviceWidth, setDeviceWidth] = useState<number>(0)
-   const confetti = false // TODO: Uncomment once it gets support for Next 14
-   // const [confetti] = useState<boolean>(localStorage.getItem("confetti") ? true : false)
-   // if (confetti) localStorage.removeItem("confetti")
 
    useEffect(() => {
-      setDeviceWidth(window.innerWidth)
       setLoading(false)
    }, [])
-
-   const config: ConfettiProps = {
-      particleCount: 150,
-      particleSize: 14,
-      duration: 3200,
-      force: 0.7,
-      width: deviceWidth + parseInt((deviceWidth / 10).toFixed(0))
-   }
 
    return !loading && (
       <div className="relative flex center h-full w-full">
          <p className="text-white">User: {username}</p>
 
-         {confetti && (<>
-            <div id="confetti-left" className="absolute top-[40%] left-0"><ConfettiExplosion {...config} /></div>
-            <div id="confetti-right" className="absolute top-[40%] right-0"><ConfettiExplosion {...config} /></div>
-         </>)}
       </div>
    )
 }
