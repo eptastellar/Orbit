9892ed6a9bf00de18e55785b730356da1e3067ab
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-02-06 20:44:20 +0100

    enhance: added comments deletion

--- web/src/routes/c/[id].ts
+++ web/src/routes/c/[id].ts
@@ -1,6 +1,7 @@
 import { checkIfSessionTokenIsValid } from "@contexts/AuthContext"
-import { uploadComment } from "@contexts/ContentContext"
-import { commentRootIdValidation, contentValidation, postIdValidation } from "@contexts/ValidationContext"
+import { deleteComment, uploadComment } from "@contexts/ContentContext"
+import { hasPermission } from "@contexts/UserContext"
+import { commentLeafIdValidation, commentRootIdValidation, contentValidation, postIdValidation } from "@contexts/ValidationContext"
 import { Request, Response } from "express"
 
 export const POST = [checkIfSessionTokenIsValid, async (req: Request, res: Response) => {
@@ -20,4 +21,26 @@ export const POST = [checkIfSessionTokenIsValid, async (req: Request, res: Respo
          }).catch((error) => { res.status(400).json({ success: false, message: error.message }) })
       }).catch((error) => { res.status(400).json({ success: false, message: error.message }) })
    } catch (error: any) { res.status(400).json({ success: false, message: error.message }) }
+}]
+
+export const DELETE = [checkIfSessionTokenIsValid, async (req: Request, res: Response) => {
+   const uid: string = res.locals.uid
+   const commentId: string = req.body.commentId
+   const postId: string = req.params.id
+   const rootId: boolean | string = req.body.rootId
+
+   postIdValidation(postId).then(async () => {
+      try {
+         if (rootId)
+            await commentRootIdValidation(commentId, postId)
+         else
+            await commentLeafIdValidation(commentId, rootId as string, postId)
+
+         hasPermission(uid, commentId, 'comments').then(() => {
+            deleteComment(commentId).then(() => {
+               res.status(200).json({ success: true })
+            }).catch((error) => { res.status(400).json({ success: false, message: error.message }) })
+         }).catch((error) => { res.status(400).json({ success: false, message: error.message }) })
+      } catch (error: any) { res.status(400).json({ success: false, message: error.message }) }
+   })
 }]
