35e1d650357aef7b01044a3acd356b299274ab16
74C17N3P7UN3 <leocalzavara05@gmail.com>
2024-03-02 12:35:05 +0100

    Dev bucket

--- app/next.config.js
+++ app/next.config.js
@@ -12,6 +12,16 @@ module.exports = {
             hostname: "storage.googleapis.com",
             pathname: "/eptastellar-orbit.appspot.com/default/**",
          },
+         {
+            protocol: "https",
+            hostname: "firebasestorage.googleapis.com",
+            pathname: "/v0/b/dev-orbit-1.appspot.com/**",
+         },
+         {
+            protocol: "https",
+            hostname: "storage.googleapis.com",
+            pathname: "/dev-orbit-1.appspot.com/default/**",
+         },
       ],
    },
 }
