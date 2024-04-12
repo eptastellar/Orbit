783e6152d29613ebb465c9e34d4aab72973eb604
TheInfernalNick <theinfernalnick@gmail.com>
2024-04-12 16:29:58 +0200

    Fixed Jest setup

--- web/src/algorithms/__mocks__/supernova.spec.ts
+++ web/src/algorithms/__mocks__/supernova.spec.ts
@@ -1,11 +1,11 @@
 import { supernova } from "../supernova"
 
-// jest.mock("@config/neo4j.config")
+jest.mock("..\\config\\neo4j.config.ts")
 
 describe("supernova function", () => {
    it("should find a suitable match", async () => {
-      const user = "Test"
-      const expectedFriend = "Friend1"
+      const user = "myName"
+      const expectedFriend = "myFriendFoundName"
 
       //TODO @TheInfernalNick prova ad eseguire supernova per vedere i risultati, comando pnpm test, devono essere mockati i moduli di neo
 
