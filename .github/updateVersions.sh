8022a09c1f69395dbef6061af229e64b6c2b2d06
TheInfernalNick <theinfernalnick@gmail.com>
2024-04-24 08:19:58 +0200

    JustTryAgain

--- .github/updateVersions.sh
+++
@@ -1,3 +0,0 @@
-VERSION=$(node -p -e "require('./package.json').version")
-NEW_VERSION=$(echo $VERSION | awk -F. -v OFS=. '{$NF++;print}')
-node -e "const pkg = require('./package.json'); pkg.version = '$NEW_VERSION'; require('fs').writeFileSync('./package.json', JSON.stringify(pkg, null, 2));"
