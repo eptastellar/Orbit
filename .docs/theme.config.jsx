225653e87718e01a92adc66643f50312101119d6
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-05-10 19:50:28 +0200

    fix: nestra setup

--- .docs/theme.config.jsx
+++ .docs/theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-   logo: <span>My Nextra Documentation</span>,
+   logo: <span>Orbit Documentation</span>,
    project: {
-      link: 'https://github.com/shuding/nextra'
+      link: 'https://github.com/eptastellar/orbit'
    }
    // ... other theme options
 }
