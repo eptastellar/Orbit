375e0813eb4d6ebaa1be515481ebfe4b903f566f
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-05-26 17:21:31 +0200

    fix: added cors

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,7 @@ import { AppModule } from '@/app.module';
 import { NestFactory } from '@nestjs/core';
 
 async function nest() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT);
 }
 nest();
