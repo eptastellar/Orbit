0da3daf274d8fadb46bfc0e290efa5525a1fa2ea
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-05-31 23:25:00 +0200

    enhance: added chats documentation

--- web/src/app.controller.ts
+++ web/src/app.controller.ts
@@ -5,7 +5,7 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 @ApiTags()
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   @ApiResponse({
