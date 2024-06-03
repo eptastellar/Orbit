6de1015d24f5372c77a9861385b28c5efb2f706b
74C17N3P7UN3 <leocalzavara05@gmail.com>
2024-06-03 20:20:15 +0200

    Added app's manifest

---
+++ app/src/app/manifest.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next"
+
+const manifest = (): MetadataRoute.Manifest => ({
+   name: "Orbit",
+   short_name: "Orbit",
+   description: "The new way to connect.",
+   start_url: "/",
+   display: "standalone",
+   icons: [
+      {
+         src: "/icon.png",
+         sizes: "256x256",
+         type: "image/png",
+      },
+   ],
+})
+
+export default manifest
